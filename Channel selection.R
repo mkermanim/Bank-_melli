c43b75414ab93ff7f30b36f28241ea6f0d7c7561
Hossein Kermani <spm.kermani@yahoo.com>
2021-01-13 10:03:22 +0330

    altered codes for channel and insta

--- Channel selection.R
+++ Channel selection.R
@@ -2,12 +2,15 @@ library(dplyr)
 library(tidyverse)
 library(pander)
 library(xlsx)
+library(readxl)
+library(persiandictionary)
 
 #reading comments file as a df 
-dfcomms <- as.data.frame(data.table::fread(file = 'comments_pod.csv'))
+channel_comms <- read_xlsx('comments_channel.xlsx', 1)
+
 
 # selecting unique usernames 
-uniqun <- dfcomms %>% select(username,count) %>%  distinct(username, .keep_all = T)
+uniqun <- channel_comms %>% select(name,mobile) %>%  distinct(name, .keep_all = T)
 
 #making a random sample and saving it 
 samplresult <- sample_n(uniqun,3)
@@ -16,6 +19,6 @@ samplresult <- sample_n(uniqun,3)
 samplresult  %>% pander(style="rmarkdown")
 
 # exporting result as a excel file
-write.xlsx(samplresult, 'result_podcast.xlsx')
+write.xlsx(samplresult, 'result_channel.xlsx')
 
 
---
+++ Read Persian char soloution.R
@@ -0,0 +1,2 @@
+#Trying this code to sole not reading Persian characters in R
+Sys.setlocale("LC_ALL", "Persian")
