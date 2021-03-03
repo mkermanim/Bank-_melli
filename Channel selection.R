1378a65f779033c59057bd6f4a5c088796938e9a
Hossein <spem.kermani@yahoo.com>
2021-03-03 10:58:13 +0330

    new changes- some modifications

--- Channel selection.R
+++ Channel selection.R
@@ -4,13 +4,14 @@ library(pander)
 library(xlsx)
 library(readxl)
 library(persiandictionary)
+library(writexl)
 
 #reading comments file as a df 
 channel_comms <- read_xlsx('comments_channel.xlsx', 1)
 
 
 # selecting unique usernames 
-uniqun <- channel_comms %>% select(name,mobile) %>%  distinct(name, .keep_all = T)
+uniqun <- channel_comms %>% filter(correct==1) %>%  select(name,mobile) %>%  distinct(name, .keep_all = T)
 
 #making a random sample and saving it 
 samplresult <- sample_n(uniqun,3)
@@ -19,6 +20,6 @@ samplresult <- sample_n(uniqun,3)
 samplresult  %>% pander(style="rmarkdown")
 
 # exporting result as a excel file
-write.xlsx(samplresult, 'result_channel.xlsx')
+write_xlsx(samplresult, 'result_channel.xlsx')
 
 
